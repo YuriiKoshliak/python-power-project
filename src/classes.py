d5b2a9b4dfd973ca6a32eb865426447162226d02
Yurii Koshliak <satorxp10@gmail.com>
2024-02-16 23:05:39 +0100

    created class Record

--- src/classes.py
+++ src/classes.py
@@ -15,16 +15,61 @@ class Phone(Field):
     ...
     # реалізація класу
 
+class Birthday(Field):
+    ...
+     # реалізація класу
+    
 class Record:
-    def __init__(self, name):
+    def __init__(self, name, birthday=None):
         self.name = Name(name)
+        self.birthday = Birthday(birthday) if birthday else None
         self.phones = []
 
-    # реалізація класу
+    def days_to_birthday(self): #Треба додати взаємодію з Birthday
+        if self.birthday:
+            ...
+        else:
+            ...
 
+    def add_phone(self, phone):
+        self.phone = Phone(phone)
+        self.phones.append(self.phone)
+
+    def add_birthday(self, date):
+        self.birthday = Birthday(date)
+
+    def remove_phone(self, phone):
+        if phone in [p.value for p in self.phones]:
+            for p in self.phones:
+                if p.value == phone:
+                    self.phones.remove(p)
+                    break
+        else:
+            raise ValueError
+
+    def edit_phone(self, old_phone, new_phone):
+        if str(old_phone) not in [p.value for p in self.phones]:
+            raise ValueError
+        for phone in self.phones:
+            if phone.value == old_phone:
+                phone.value = new_phone
+                break
+        
+    def find_phone(self, phone):
+        phone = str(phone)
+        if phone in [p.value for p in self.phones]:
+            return Phone(phone)
+        else:
+            return None
+         
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+        return (f"Contact name: {self.name.value}, "
+                f"phones: {'; '.join(p.value for p in self.phones) if self.phones else None}, "
+                f"birthday: {self.birthday.value if self.birthday else None}")
 
+
+
+   
 class AddressBook(UserDict):
     ...
     # реалізація класу
